7006e630261de2d809e614121fca125f9cb77522
Paull0909 <165283405+Paull0909@users.noreply.github.com>
2025-05-07 21:04:28 +0700

    kkkk

--- CoffeeAPI/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ CoffeeAPI/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b43bfe70b1b3ed5a4a5545f81fbbd5a9743f66c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
